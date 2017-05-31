d57a52c1658d350ab12689965dce7b07828f0412
Linus Aldebjer <aldebjer@gmail.com>
2017-05-31 16:17:51 +0200

    fixing name of tests

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/{{cookiecutter.system_collection_slug}}_test.py
@@ -0,0 +1,5 @@
+"""Some tests for the {{cookiecutter.system_collection_slug}} systems"""
+
+def test_importof_{{cookiecutter.system_collection_slug}}():
+    import {{cookiecutter.project_slug}}.systems.{{cookiecutter.system_collection_slug}}
+
